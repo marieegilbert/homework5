044f7fa61f52669d367f1a7640f9d1d9bb7f8666
Gilbert <Marie.Gilbert@sony.com>
2019-10-19 15:59:02 -0700

    updated script and html file

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ $(document).ready(function(){
     console.log($(this).siblings(".description").val());
     var textValue = $(this).siblings(".description").val();
     localStorage.setItem(time, textValue);
-    localStorage.setItem(time, textValue);
+  // Get stored time from localStorage
+  // Parsing the JSON string to an object
+  var storedTimes = JSON.parse(localStorage.getItem(time));
+  console.log(storedTimes);
+  // If time was retrieved from localStorage, update the time var to it
+  if (storedTimes !== null) {
+    time = storedTimes;
+  }
   })
 })
 
-var currentHour = 12;
-var array = [1]
-for (var currentHour = i; i < Array.length; i++)
+//The $.now() method is a shorthand for the number returned by the expression (new Date).getTime().
+// var currentHour = 12;
+// var array = [1]
+// for (var currentHour = i; i < Array.length; i++)
